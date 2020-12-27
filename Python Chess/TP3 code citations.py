c1e68bc5099d04072078f97833cf8a19bf53c430
vishnuraghavan2020 <59420089+vishnuraghavan2020@users.noreply.github.com>
2020-12-27 17:59:53 -0500

    Add files via upload

---
+++ Python Chess/TP3 code citations.py
@@ -0,0 +1,13 @@
+"""
+Code citations
+
+Animation "Run" function from https://www.cs.cmu.edu/~112-n19/notes/notes-animations-part2.html
+
+
+
+Chess pieces:
+https://commons.wikimedia.org/wiki/Category:PNG_chess_pieces/Standard_transparent
+Stored in "chess_pieces" folder inside TP3 Vishnu
+
+
+"""
